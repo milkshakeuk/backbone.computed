72bce90e232566a8d9fa408f2d1e443c6b7bd5d8
Jake Aitchison <webmaster@liquidfists.com>
2016-02-17 09:01:36 +0000

    added more tests plus basic README and travis-ci status

---
+++ README.md
@@ -0,0 +1,5 @@
+# backbone.computed [![Build Status][travis-image]][travis-url]
+Simple computed fields for Backbone models
+
+[travis-url]: https://travis-ci.org/milkshakeuk/backbone.computed
+[travis-image]: https://travis-ci.org/milkshakeuk/backbone.computed.svg?branch=master
--- test/computed.spec.js
+++ test/computed.spec.js
@@ -1,10 +1,15 @@
 define(['backbone', 'underscore'], function (Backbone, _) {
 
+  /**
+   * prevent ajax calls being made during testing
+   */
+  Backbone.ajax = function() {};
+
   describe('A Model', function () {
 
-    describe('With computed properties', function () {
+    describe('Which has computed properties', function () {
 
-      it('Can be accessed by `get`', function () {
+      it('Can access them using `get`', function () {
         var Model = Backbone.Model.extend({
           computed: {
             fullName: function () {
@@ -19,7 +24,37 @@ define(['backbone', 'underscore'], function (Backbone, _) {
         });
 
         expect(model.get('fullName')).toBe('Tim Burton');
-      })
+      });
+
+      it('Will not serialise them when `save` is invoked', function (done) {
+        var Model = Backbone.Model.extend({
+          url: '/test',
+          toJSON: function () {
+            var data = Backbone.Model.prototype.toJSON.apply(this, arguments);
+            expect(data.fullName).toBe(undefined);
+            done();
+            return data;
+          }
+        });
+        var model = new Model;
+        model.save({ firstName: 'Tim', lastName: 'Burton' });
+      });
+
+      it('Will serialise them when `toJSON` is invoked independant of `save`', function () {
+        var Model = Backbone.Model.extend({
+          computed: {
+            fullName: function () {
+              return this.get('firstName') + ' ' + this.get('lastName');
+            }
+          }
+        });
+        var model = new Model({
+          firstName: 'Tim',
+          lastName: 'Burton'
+        });
+        var result = _.isEqual(model.toJSON(), { firstName: 'Tim', lastName: 'Burton', fullName: 'Tim Burton' });
+        expect(result).toBe(true);
+      });
 
     });
 
